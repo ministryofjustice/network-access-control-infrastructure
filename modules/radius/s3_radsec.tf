f78c8338557bef95e8ab5369525d6d8d60512ccb
Emile <emile.swarts@digital.justice.gov.uk>
2021-06-17 11:50:50 +0100

    Create s3 bucket for Radsec and radius certificates

--- modules/radius/s3_radsec.tf
+++ modules/radius/s3_radsec.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "config_bucket" {
-  bucket = "${var.prefix}-certificate-bucket"
+resource "aws_s3_bucket" "radsec_certificate_bucket" {
+  bucket = "${var.prefix}-radsec-certificate-bucket"
   acl    = "private"
   versioning {
     enabled = true
@@ -8,30 +8,30 @@ resource "aws_s3_bucket" "config_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.config_bucket_key.arn
+        kms_master_key_id = aws_kms_key.radsec_bucket_key.arn
         sse_algorithm     = "aws:kms"
       }
     }
   }
 }
 
-data "template_file" "config_bucket_policy" {
+data "template_file" "radsec_bucket_policy" {
   template = file("${path.module}/policies/bucket_policy.json")
 
   vars = {
-    bucket_arn = aws_s3_bucket.config_bucket.arn,
+    bucket_arn = aws_s3_bucket.radsec_certificate_bucket.arn,
     ecs_task_role_arn = aws_iam_role.ecs_task_role.arn
   }
 }
 
-resource "aws_s3_bucket_policy" "config_bucket_policy" {
-  bucket = aws_s3_bucket.config_bucket.id
+resource "aws_s3_bucket_policy" "radsec_bucket_policy" {
+  bucket = aws_s3_bucket.radsec_certificate_bucket.id
 
-  policy = data.template_file.config_bucket_policy.rendered
+  policy = data.template_file.radsec_bucket_policy.rendered
 }
 
-resource "aws_s3_bucket_public_access_block" "config_bucket_public_block" {
-  bucket = aws_s3_bucket.config_bucket.id
+resource "aws_s3_bucket_public_access_block" "radsec_bucket_public_block" {
+  bucket = aws_s3_bucket.radsec_certificate_bucket.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -39,7 +39,7 @@ resource "aws_s3_bucket_public_access_block" "config_bucket_public_block" {
   restrict_public_buckets = true
 }
 
-resource "aws_kms_key" "config_bucket_key" {
+resource "aws_kms_key" "radsec_bucket_key" {
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
   enable_key_rotation     = true
