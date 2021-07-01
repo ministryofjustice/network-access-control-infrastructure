9a56c29497068a26a1bfabb853bad2b5ab0087f2
Emile <emile.swarts@digital.justice.gov.uk>
2021-07-01 11:56:09 +0100

    Enable Federated SSO for admin application in development

--- main.tf
+++ main.tf
@@ -121,13 +121,13 @@ module "admin" {
   vpn_hosted_zone_id                   = var.vpn_hosted_zone_id
   vpn_hosted_zone_domain               = var.vpn_hosted_zone_domain
   admin_db_backup_retention_period     = var.admin_db_backup_retention_period
-  # cognito_user_pool_id                 = module.authentication.cognito_user_pool_id
-  # cognito_user_pool_domain             = module.authentication.cognito_user_pool_domain
-  # cognito_user_pool_client_id          = module.authentication.cognito_user_pool_client_id
-  # cognito_user_pool_client_secret      = module.authentication.cognito_user_pool_client_secret
   radius_cluster_name                  = module.radius.ecs.cluster_name
   radius_service_name                  = module.radius.ecs.service_name
   radius_service_arn                   = module.radius.ecs.service_arn
+  cognito_user_pool_id                 = module.authentication.cognito_user_pool_id
+  cognito_user_pool_domain             = module.authentication.cognito_user_pool_domain
+  cognito_user_pool_client_id          = module.authentication.cognito_user_pool_client_id
+  cognito_user_pool_client_secret      = module.authentication.cognito_user_pool_client_secret
   is_publicly_accessible               = local.publicly_accessible
   admin_local_development_domain_affix = var.admin_local_development_domain_affix
 
@@ -144,19 +144,19 @@ locals {
   publicly_accessible = terraform.workspace == "production" ? false : true
 }
 
-# module "authentication" {
-#   source                        = "./modules/cognito"
-#   azure_federation_metadata_url = var.azure_federation_metadata_url
-#   prefix                        = module.label.id
-#   enable_authentication         = var.enable_authentication
-#   admin_url                     = module.admin.admin_url
-#   region                        = data.aws_region.current_region.id
-#   vpn_hosted_zone_domain        = var.vpn_hosted_zone_domain
+module "authentication" {
+  source                        = "./modules/cognito"
+  azure_federation_metadata_url = var.azure_federation_metadata_url
+  prefix                        = module.label.id
+  enable_authentication         = var.enable_authentication
+  admin_url                     = module.admin.admin_url
+  region                        = data.aws_region.current_region.id
+  vpn_hosted_zone_domain        = var.vpn_hosted_zone_domain
 
-#   providers = {
-#     aws = aws.env
-#   }
-# }
+  providers = {
+    aws = aws.env
+  }
+}
 
 data "aws_region" "current_region" {}
 data "aws_caller_identity" "shared_services_account" {}
