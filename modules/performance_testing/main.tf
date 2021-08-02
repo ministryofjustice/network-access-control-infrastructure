0a589fa2075191da2cea7a17da35344e5891580d
Emile <emile.swarts@digital.justice.gov.uk>
2021-08-02 08:07:11 +0100

    Restore performance testing module
    Also reduce instance size for now

--- modules/performance_testing/main.tf
+++ modules/performance_testing/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "performance_testing_instance" {
   ami           = "ami-0b3228cca3203786f"
-  instance_type = "t4g.large"
+  instance_type = "t2.medium"
 
   vpc_security_group_ids = [
     aws_security_group.performance_testing_instance.id
