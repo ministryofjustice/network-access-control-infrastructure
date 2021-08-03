577979d04fd628ba0bdb8612acbd7a5574db47d8
Emile <emile.swarts@digital.justice.gov.uk>
2021-08-03 11:16:05 +0100

    Introduce 3rd performance testing instance
    This distributes the load more evenly

--- modules/performance_testing/main.tf
+++ modules/performance_testing/main.tf
@@ -40,6 +40,27 @@ resource "aws_instance" "performance_testing_instance2" {
   }
 }
 
+resource "aws_instance" "performance_testing_instance3" {
+  ami           = "ami-07438ed9014cde68f"
+  instance_type = "t4g.large"
+
+  vpc_security_group_ids = [
+    aws_security_group.performance_testing_instance.id
+  ]
+
+  subnet_id                   = var.subnets[1]
+  key_name                    = aws_key_pair.performance_testing_public_key_pair.key_name
+  monitoring                  = true
+  associate_public_ip_address = true
+  iam_instance_profile = aws_iam_instance_profile.ec2_perf_test_profile.name
+  instance_initiated_shutdown_behavior = "terminate"
+  user_data = data.template_cloudinit_config.config.rendered
+
+  tags = {
+    Name = "MoJ Authentication POC performance"
+  }
+}
+
 data "template_file" "client" {
   template = file("./modules/performance_testing/user_data.sh")
   vars = {
