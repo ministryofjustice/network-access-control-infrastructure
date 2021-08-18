15852f5d5c76fcaede8304176162ae553d8e6a0b
Emile <emile.swarts@digital.justice.gov.uk>
2021-08-18 13:47:00 +0100

    Assign CNAME to Network Load Balancer
    A DNS record is required to get a letsencrypt certificate

---
+++ modules/radius/letsencrypt.tf
@@ -0,0 +1,27 @@
+resource "aws_acm_certificate" "nac_nlb" {
+  domain_name       = "server.${var.env}.nac.justice.gov.uk"
+  validation_method = "DNS"
+
+  tags = var.tags
+}
+
+resource "aws_acm_certificate_validation" "nac_nlb" {
+  certificate_arn         = aws_acm_certificate.nac_nlb.arn
+  validation_record_fqdns = [for record in aws_route53_record.nac_nlb : record.fqdn]
+}
+
+resource "aws_route53_record" "nac_nlb" {
+  for_each = {
+    for dvo in aws_acm_certificate.nac_nlb.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  records = [each.value.record]
+  ttl     = 3600
+  type    = each.value.type
+  zone_id = var.hosted_zone_id
+}
