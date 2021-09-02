fe50b3b273c4aecbef2421668485eabf35057821
Emile <emile.swarts@digital.justice.gov.uk>
2021-09-02 12:29:55 +0100

    Create private subnets in the admin VPC
    We will move the admin database to private subnets for security.
    Put in public subnets initially for debugging.

--- modules/admin_vpc/outputs.tf
+++ modules/admin_vpc/outputs.tf
@@ -6,6 +6,10 @@ output "public_subnets" {
   value = module.vpc.public_subnets
 }
 
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
 output "public_subnet_cidr_blocks" {
   value = module.vpc.public_subnets_cidr_blocks
 }
