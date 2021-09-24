aebf628b097f9dabadaf75cc3fb8610d1b1b23dc
Emile <emile.swarts@digital.justice.gov.uk>
2021-09-24 10:52:07 +0100

    Temporarily allow all https traffic
    This is required to rebuild the vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "endpoints" {
     protocol    = "tcp"
     from_port   = 443
     to_port     = 443
-    cidr_blocks = [var.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
