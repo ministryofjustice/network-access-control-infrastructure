dfc8d208f4d402e01a2f4819efe1a0c8504c85a5
Emile <emile.swarts@digital.justice.gov.uk>
2021-09-24 14:16:50 +0100

    Restore transit gateway attachment

--- modules/vpc/routes.tf
+++ modules/vpc/routes.tf
@@ -1,47 +1,47 @@
-# resource "aws_route" "transit-gateway" {
-#   for_each = var.enable_nac_transit_gateway_attachment ? toset(module.vpc.private_route_table_ids) : []
+resource "aws_route" "transit-gateway" {
+  for_each = var.enable_nac_transit_gateway_attachment ? toset(module.vpc.private_route_table_ids) : []
 
-#   route_table_id         = each.value
-#   destination_cidr_block = "0.0.0.0/0"
-#   transit_gateway_id     = var.transit_gateway_id
+  route_table_id         = each.value
+  destination_cidr_block = "0.0.0.0/0"
+  transit_gateway_id     = var.transit_gateway_id
 
-#   depends_on = [
-#     module.vpc
-#   ]
-# }
+  depends_on = [
+    module.vpc
+  ]
+}
 
-# resource "aws_route" "transit-gateway-public" {
-#   for_each = var.enable_nac_transit_gateway_attachment ? toset(module.vpc.public_route_table_ids) : []
+resource "aws_route" "transit-gateway-public" {
+  for_each = var.enable_nac_transit_gateway_attachment ? toset(module.vpc.public_route_table_ids) : []
 
-#   route_table_id         = each.value
-#   destination_cidr_block = "${var.ocsp_endpoint_ip}/32"
-#   transit_gateway_id     = var.transit_gateway_id
+  route_table_id         = each.value
+  destination_cidr_block = "${var.ocsp_endpoint_ip}/32"
+  transit_gateway_id     = var.transit_gateway_id
 
-#   depends_on = [
-#     module.vpc
-#   ]
-# }
+  depends_on = [
+    module.vpc
+  ]
+}
 
-# resource "aws_route" "transit-gateway-public-dns-server-1" {
-#   for_each = var.enable_nac_transit_gateway_attachment ? toset(module.vpc.public_route_table_ids) : []
+resource "aws_route" "transit-gateway-public-dns-server-1" {
+  for_each = var.enable_nac_transit_gateway_attachment ? toset(module.vpc.public_route_table_ids) : []
 
-#   route_table_id         = each.value
-#   destination_cidr_block = "${var.mojo_dns_ip_1}/32"
-#   transit_gateway_id     = var.transit_gateway_id
+  route_table_id         = each.value
+  destination_cidr_block = "${var.mojo_dns_ip_1}/32"
+  transit_gateway_id     = var.transit_gateway_id
 
-#   depends_on = [
-#     module.vpc
-#   ]
-# }
+  depends_on = [
+    module.vpc
+  ]
+}
 
-# resource "aws_route" "transit-gateway-public-dns-server-2" {
-#   for_each = var.enable_nac_transit_gateway_attachment ? toset(module.vpc.public_route_table_ids) : []
+resource "aws_route" "transit-gateway-public-dns-server-2" {
+  for_each = var.enable_nac_transit_gateway_attachment ? toset(module.vpc.public_route_table_ids) : []
 
-#   route_table_id         = each.value
-#   destination_cidr_block = "${var.mojo_dns_ip_2}/32"
-#   transit_gateway_id     = var.transit_gateway_id
+  route_table_id         = each.value
+  destination_cidr_block = "${var.mojo_dns_ip_2}/32"
+  transit_gateway_id     = var.transit_gateway_id
 
-#   depends_on = [
-#     module.vpc
-#   ]
-# }
+  depends_on = [
+    module.vpc
+  ]
+}
--- modules/vpc/transit_gateway_attachment.tf
+++ modules/vpc/transit_gateway_attachment.tf
@@ -1,26 +1,26 @@
-# resource "aws_ec2_transit_gateway_vpc_attachment" "nac_transit_gateway_attachment" {
-#   count = var.enable_nac_transit_gateway_attachment ? 1 : 0
+resource "aws_ec2_transit_gateway_vpc_attachment" "nac_transit_gateway_attachment" {
+  count = var.enable_nac_transit_gateway_attachment ? 1 : 0
 
-#   subnet_ids                                      = module.vpc.private_subnets
-#   transit_gateway_id                              = var.transit_gateway_id
-#   vpc_id                                          = module.vpc.vpc_id
-#   transit_gateway_default_route_table_association = false
-#   transit_gateway_default_route_table_propagation = false
-#   tags                                            = var.tags
+  subnet_ids                                      = module.vpc.private_subnets
+  transit_gateway_id                              = var.transit_gateway_id
+  vpc_id                                          = module.vpc.vpc_id
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+  tags                                            = var.tags
 
-#   depends_on = [
-#     module.vpc
-#   ]
-# }
+  depends_on = [
+    module.vpc
+  ]
+}
 
-# resource "aws_ec2_transit_gateway_route_table_association" "this" {
-#   count                          = var.enable_nac_transit_gateway_attachment ? 1 : 0
-#   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.nac_transit_gateway_attachment[0].id
-#   transit_gateway_route_table_id = var.transit_gateway_route_table_id
-# }
+resource "aws_ec2_transit_gateway_route_table_association" "this" {
+  count                          = var.enable_nac_transit_gateway_attachment ? 1 : 0
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.nac_transit_gateway_attachment[0].id
+  transit_gateway_route_table_id = var.transit_gateway_route_table_id
+}
 
-# resource "aws_ec2_transit_gateway_route_table_propagation" "this" {
-#   count                          = var.enable_nac_transit_gateway_attachment ? 1 : 0
-#   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.nac_transit_gateway_attachment[0].id
-#   transit_gateway_route_table_id = var.transit_gateway_route_table_id
-# }
+resource "aws_ec2_transit_gateway_route_table_propagation" "this" {
+  count                          = var.enable_nac_transit_gateway_attachment ? 1 : 0
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.nac_transit_gateway_attachment[0].id
+  transit_gateway_route_table_id = var.transit_gateway_route_table_id
+}
