230d79ed664ce41dc324708b6fdf93f691dc94ed
csabagyorfi <csaba@madetech.com>
2021-09-29 12:22:45 +0100

    Revert "Temp remove perf testing intance"
    This reverts commit 03fb452ea05ca8d709641379ac6510204aa1b49f.

--- modules/performance_testing/main.tf
+++ modules/performance_testing/main.tf
@@ -1,23 +1,23 @@
-# resource "aws_instance" "performance_testing_instance" {
-#   ami           = "ami-07438ed9014cde68f"
-#   instance_type = "t4g.medium"
+resource "aws_instance" "performance_testing_instance" {
+  ami           = "ami-07438ed9014cde68f"
+  instance_type = "t4g.medium"
 
-#   vpc_security_group_ids = [
-#     aws_security_group.performance_testing_instance.id
-#   ]
+  vpc_security_group_ids = [
+    aws_security_group.performance_testing_instance.id
+  ]
 
-#   subnet_id                   = var.subnets[0]
-#   key_name                    = aws_key_pair.performance_testing_public_key_pair.key_name
-#   monitoring                  = true
-#   associate_public_ip_address = true
-#   iam_instance_profile = aws_iam_instance_profile.ec2_perf_test_profile.name
-#   instance_initiated_shutdown_behavior = "terminate"
-#   user_data = data.template_cloudinit_config.config.rendered
+  subnet_id                   = var.subnets[0]
+  key_name                    = aws_key_pair.performance_testing_public_key_pair.key_name
+  monitoring                  = true
+  associate_public_ip_address = true
+  iam_instance_profile = aws_iam_instance_profile.ec2_perf_test_profile.name
+  instance_initiated_shutdown_behavior = "terminate"
+  user_data = data.template_cloudinit_config.config.rendered
 
-#   tags = {
-#     Name = "MoJ Authentication Performance"
-#   }
-# }
+  tags = {
+    Name = "MoJ Authentication Performance"
+  }
+}
 
 data "template_file" "client" {
   template = file("./modules/performance_testing/user_data.sh")
