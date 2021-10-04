28e58597690daab15a7edc01831d16f8b4fe2b0e
MichaelCullenMadeTech <michael.cullen@madetech.com>
2021-10-04 09:57:29 +0100

    Create the network-access-service zone separate (unknown error on changing original).

---
+++ modules/radius/route53_radsec.tf
@@ -0,0 +1,15 @@
+
+locals {
+  zone_prefixes = {
+    "development" = "dev.",
+    "pre-production" = "prep."
+  }
+}
+
+resource "aws_route53_zone" "radius_service" {
+  count = var.enable_hosted_zone ? 1 : 0
+
+  name = "${lookup(local.zone_prefixes, var.env, "")}network-access-control.service.justice.gov.uk"
+
+  tags = var.tags
+}
