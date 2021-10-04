698deed1862426f9ee19f7fc86ced57750b84a98
MichaelCullenMadeTech <79979174+MichaelCullenMadeTech@users.noreply.github.com>
2021-10-04 09:23:30 +0100

    Revert "Radsec route53 (#54)"
    This reverts commit 79691f8a2932fc2d5abab390435bffbd84876fae.

--- modules/radius/route53.tf
+++ modules/radius/route53.tf
@@ -8,7 +8,7 @@ locals {
 resource "aws_route53_zone" "radius" {
   count = var.enable_hosted_zone ? 1 : 0
 
-  name = "${lookup(local.zone_prefixes, var.env, "")}network-access-control.service.justice.gov.uk"
+  name = "${lookup(local.zone_prefixes, var.env, "")}nac.justice.gov.uk"
 
   tags = var.tags
 }
