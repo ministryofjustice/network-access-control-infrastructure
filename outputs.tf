202c065c606e1b129a1a3b4dc2db2e88936088eb
Yusuf <yusuf@madetech.com>
2021-10-25 12:44:30 +0100

    Add RADIUS S3 buckets to Terraform outputs

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,7 @@ output "terraform_outputs" {
     radius = {
       ecs = module.radius.ecs
       ecr = module.radius.ecr
+      s3 = module.radius.s3
     }
     admin = {
       ecr = module.admin.ecr
