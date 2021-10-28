21e8bd916798991ed4c023635115e8448ad927af
Emile Swarts <emile.swarts123@gmail.com>
2021-10-28 15:29:31 +0100

    Remove geo-location from radsec dns entry (#79)
    This may cause issues down the line, fine to resolve from anywhere

--- modules/radius/route53_radsec.tf
+++ modules/radius/route53_radsec.tf
@@ -2,11 +2,6 @@ resource "aws_route53_record" "radsec" {
   zone_id = var.hosted_zone_id
   ttl     = 3600
   type    = "CNAME"
-  set_identifier = "geolocation"
-  geolocation_routing_policy {
-    country = "GB"
-  }
-
   name    = "radsec${var.local_development_domain_affix}"
   records = [aws_lb.load_balancer.dns_name]
   allow_overwrite = true
