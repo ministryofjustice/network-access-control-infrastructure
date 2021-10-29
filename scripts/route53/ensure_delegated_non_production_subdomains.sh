cc60597c7d4961b03de1b83d1a0da53a13d6660e
Emile <emile.swarts@digital.justice.gov.uk>
2021-10-29 17:18:28 +0100

    Inline authentication into the same route53 function

--- scripts/route53/ensure_delegated_non_production_subdomains.sh
+++ scripts/route53/ensure_delegated_non_production_subdomains.sh
@@ -2,19 +2,15 @@
 
 set -e
 
-assume_role_target_aws_account() {
-  env
-
+ensure_subdomain_ns_records() {
   TEMP_ROLE=`aws sts assume-role --role-arn $1 --role-session-name ci-build-$CODEBUILD_BUILD_NUMBER`
   export AWS_ACCESS_KEY_ID=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.AccessKeyId')
   export AWS_SECRET_ACCESS_KEY=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SecretAccessKey')
   export AWS_SESSION_TOKEN=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SessionToken')
-}
 
-ensure_subdomain_ns_records() {
   aws logs describe-log-groups
 
-  # name_servers=$(aws route53 list-resource-record-sets --hosted-zone-id $1 |jq '.ResourceRecordSets[]' | jq 'select(.Type=="NS")') > ns.json
+  # name_servers=$(aws route53 list-resource-record-sets --hosted-zone-id $2 |jq '.ResourceRecordSets[]' | jq 'select(.Type=="NS")') > ns.json
   # name=$(jq -r '.Name' ns.json)
   # jq ".Name = \"${name}\"" record_set_template.json
   # jq ".Changes[0].ResourceRecordSet = ${name_servers}" record_set_template.json > upsert.json
@@ -25,11 +21,9 @@ ensure_subdomain_ns_records() {
 }
 
 main() {
-  assume_role_target_aws_account $DEVELOPMENT_ASSUME_ROLE
-  ensure_subdomain_ns_records $DEVELOPMENT_HOSTED_ZONE_ID
+  ensure_subdomain_ns_records $DEVELOPMENT_ASSUME_ROLE $DEVELOPMENT_HOSTED_ZONE_ID
 
-  assume_role_target_aws_account $PRE_PRODUCTION_ASSUME_ROLE
-  ensure_subdomain_ns_records $PRE_PRODUCTION_HOSTED_ZONE_ID
+  ensure_subdomain_ns_records $PRE_PRODUCTION_ASSUME_ROLE $PRE_PRODUCTION_HOSTED_ZONE_ID
 }
 
 # if [ "$ENV" == "production" ]; then
