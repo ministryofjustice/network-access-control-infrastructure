3f763594fdaa369a394537e206c09e69b0c17f66
Emile <emile.swarts@digital.justice.gov.uk>
2021-10-29 16:17:19 +0100

    Create nameservers in production AWS account for subdomains
    The TLD hosted zone is in the productin AWS account. To support non-live
    environment sub domains, a NS record needs to be created in the
    production account for each of the non-live environments (development,
    pre-production).
    
    Automate this by running a script on the production build pipeline,
    which will:
    1. Get the nameservers for dev, prep
    2. Create an upsert json payload
    3. Apply this to production route53 using the aws cli
    
    This will be idempotent and can be run for each build, if the records
    exist, it will be a noop.

---
+++ scripts/route53/ensure_delegated_non_production_subdomains.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+assume_role_target_aws_account() {
+  TEMP_ROLE=`aws sts assume-role --role-arn $1 --role-session-name ci-build-$CODEBUILD_BUILD_NUMBER`
+  export AWS_ACCESS_KEY_ID=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.AccessKeyId')
+  export AWS_SECRET_ACCESS_KEY=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SecretAccessKey')
+  export AWS_SESSION_TOKEN=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SessionToken')
+}
+
+ensure_subdomain_ns_records() {
+  name_servers=$(aws route53 list-resource-record-sets --hosted-zone-id $hosted_zone_id |jq '.ResourceRecordSets[]' | jq 'select(.Type=="NS")') > ns.json
+  name=$(jq -r '.Name' ns.json)
+  jq ".Name = \"${name}\"" record_set_template.json
+  jq ".Changes[0].ResourceRecordSet = ${name_servers}" record_set_template.json > upsert.json
+
+  cat upsert.json
+
+  #aws route53 change-resource-record-sets --hosted-zone-id $hosted_zone_id --change-batch file://upsert.json
+}
+
+main() {
+  assume_role_target_aws_account
+  ensure_subdomain_ns_records $DEVELOPMENT_ASSUME_ROLE $DEVELOPMENT_HOSTED_ZONE_ID
+  ensure_subdomain_ns_records PRE_PRODUCTION_ASSUME_ROLE PRE_PRODUCTION_HOSTED_ZONE_ID
+}
+
+if [ "$ENV" == "production" ]; then
+  main
+fi
+
