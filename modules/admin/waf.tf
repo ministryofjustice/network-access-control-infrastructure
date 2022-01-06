14a5f94387f7e48ff06def468da1785c8d58b0be
Emile <emile.swarts@digital.justice.gov.uk>
2022-01-06 13:58:16 +0000

    Fix rule syntax to allow access to admin from GB

--- modules/admin/waf.tf
+++ modules/admin/waf.tf
@@ -15,20 +15,13 @@ resource "aws_wafv2_web_acl" "admin_alb_acl" {
   rule {
     name     = "only-gb"
     priority = 1
-    override_action {
-      count {}
+    action {
+      allow {}
     }
 
     statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesCommonRuleSet"
-        vendor_name = "AWS"
-
-        scope_down_statement {
-          geo_match_statement {
-            country_codes = ["GB"]
-          }
-        }
+      geo_match_statement {
+        country_codes = ["GB"]
       }
     }
 
