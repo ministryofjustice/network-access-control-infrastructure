23b153c1de7f8d09d96a26e724e546e9a0e5c394
Emile <emile.swarts@digital.justice.gov.uk>
2022-01-07 11:38:17 +0000

    Fine tune auto scaling threshold radius

--- modules/ecs_auto_scaling_radius/main.tf
+++ modules/ecs_auto_scaling_radius/main.tf
@@ -50,7 +50,7 @@ resource "aws_cloudwatch_metric_alarm" "packets_high" {
   alarm_name                = "${var.prefix}-packets-per-container-high"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "2"
-  threshold                 = "35000"
+  threshold                 = "10000"
   alarm_description         = "Packets processed per container"
   insufficient_data_actions = []
 
@@ -101,7 +101,7 @@ resource "aws_cloudwatch_metric_alarm" "packets_low" {
   alarm_name                = "${var.prefix}-packets-per-container-low"
   comparison_operator       = "LessThanThreshold"
   evaluation_periods        = "2"
-  threshold                 = "20000"
+  threshold                 = "8000"
   alarm_description         = "Packets processed per container"
   insufficient_data_actions = []
 
