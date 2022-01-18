5fe6e4400a680deae387ecc746a9f7c992b79d99
Emile <emile.swarts@digital.justice.gov.uk>
2022-01-18 09:15:04 +0000

    Don't generate public / private key for performance testing instances
    There is no need to SSH into the performance testing instances,
    bootstrap scripts kick off the tests.

--- modules/performance_testing/security_groups.tf
+++ modules/performance_testing/security_groups.tf
@@ -1,33 +1,12 @@
 resource "aws_security_group" "performance_testing_instance" {
   name        = "performance-testing-instance"
-  description = "Allow SSH into Performance Testing instance"
+  description = "Performance testing instance"
   vpc_id      = var.vpc_id
-
-  egress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   egress {
       from_port = 1812
       to_port   = 1812
       protocol  = "udp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-      from_port = 1814
-      to_port   = 1814
-      protocol  = "udp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-      from_port = 2083
-      to_port   = 2083
-      protocol  = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -44,33 +23,4 @@ resource "aws_security_group" "performance_testing_instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-      from_port = 1812
-      to_port   = 1812
-      protocol  = "udp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-      from_port = 1814
-      to_port   = 1814
-      protocol  = "udp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-      from_port = 2083
-      to_port   = 2083
-      protocol  = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
 }
