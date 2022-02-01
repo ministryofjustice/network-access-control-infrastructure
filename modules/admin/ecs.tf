17ad678369ad12d311cd8514c6bea8f90156bf97
Emile Swarts <emile.swarts123@gmail.com>
2022-02-01 10:47:36 +0000

    Create admin background workers service (#134)
    This will be used to do CSV imports asynchronously instead of on the
    main thread which is slow.

--- modules/admin/ecs.tf
+++ modules/admin/ecs.tf
@@ -183,6 +183,136 @@ EOF
   tags = var.tags
 }
 
+resource "aws_ecs_task_definition" "admin_background_worker_task" {
+  family                   = "${var.prefix}-task"
+  requires_compatibilities = ["FARGATE"]
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  cpu                      = "512"
+  memory                   = "2048"
+  network_mode             = "awsvpc"
+
+  container_definitions = <<EOF
+[
+    {
+      "essential": true,
+      "name": "admin",
+      "command": ["bundle", "exec", "rake", "jobs:work"],
+      "environment": [
+        {
+          "name": "DB_USER",
+          "value": "${var.db.username}"
+        },{
+          "name": "DB_PASS",
+          "value": "${var.db.password}"
+        },{
+          "name": "DB_NAME",
+          "value": "${aws_db_instance.admin_db.name}"
+        },{
+          "name": "DB_HOST",
+          "value": "${aws_db_instance.admin_db.address}"
+        },{
+          "name": "RACK_ENV",
+          "value": "production"
+        },{
+          "name": "RAILS_ENV",
+          "value": "production"
+        },{
+          "name": "SECRET_KEY_BASE",
+          "value": "${var.secret_key_base}"
+        },{
+          "name": "RAILS_LOG_TO_STDOUT",
+          "value": "1"
+        },{
+          "name": "RAILS_SERVE_STATIC_FILES",
+          "value": "1"
+        },{
+          "name": "SENTRY_DSN",
+          "value": "${var.sentry_dsn}"
+        },{
+          "name": "COGNITO_CLIENT_ID",
+          "value": "${var.cognito_user_pool_client_id}"
+        },
+        {
+          "name": "COGNITO_CLIENT_SECRET",
+          "value": "${var.cognito_user_pool_client_secret}"
+        },
+        {
+          "name": "COGNITO_USER_POOL_SITE",
+          "value": "https://${var.cognito_user_pool_domain}.auth.${var.region}.amazoncognito.com"
+        },
+        {
+          "name": "COGNITO_USER_POOL_ID",
+          "value": "${var.cognito_user_pool_id}"
+        },
+        {
+          "name": "RADIUS_CLUSTER_NAME",
+          "value": "${var.radius_cluster_name}"
+        },
+        {
+          "name": "RADIUS_SERVICE_NAME",
+          "value": "${var.radius_service_name}"
+        },
+        {
+          "name": "RADIUS_INTERNAL_SERVICE_NAME",
+          "value": "${var.radius_internal_service_name}"
+        },
+        {
+          "name": "RADIUS_CONFIG_BUCKET_NAME",
+          "value": "${var.radius_config_bucket_name}"
+        },
+        {
+          "name": "RADIUS_CERTIFICATE_BUCKET_NAME",
+          "value": "${var.radius_certificate_bucket_name}"
+        },
+        {
+          "name": "CLOUDWATCH_LINK",
+          "value": "${var.cloudwatch_link}"
+        }
+      ],
+      "image": "${aws_ecr_repository.admin_ecr.repository_url}",
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-group": "${aws_cloudwatch_log_group.admin_log_group.name}",
+          "awslogs-region": "${var.region}",
+          "awslogs-stream-prefix": "${var.prefix}-docker-logs"
+        }
+      },
+      "expanded": true
+    }
+]
+EOF
+
+  tags = var.tags
+}
+
+resource "aws_ecs_service" "admin_background_worker_service" {
+  name            = "admin-background-workers"
+  cluster         = var.radius_cluster_id
+  task_definition = aws_ecs_task_definition.admin_background_worker_task.arn
+  desired_count   = 3
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets = var.vpc.public_subnets
+
+    security_groups = [
+      aws_security_group.admin_ecs.id
+    ]
+
+    assign_public_ip = false
+  }
+
+  lifecycle {
+    ignore_changes = [
+      desired_count
+    ]
+  }
+
+  tags = var.tags
+}
+
 resource "aws_ecs_service" "admin_service" {
   depends_on      = [aws_alb_listener.alb_listener]
   name            = "admin"
