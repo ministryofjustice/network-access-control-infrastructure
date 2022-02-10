aa68c8a464afba68d65198964a0a56c3a3db5b1a
Emile <emile.swarts@digital.justice.gov.uk>
2022-02-10 10:04:14 +0000

    Send logs to temporary log group
    Performance testing will generate a large volume of logs, send this to a
    temporary log group and point the metrics at this log group for
    visibility in Grafana.

--- modules/radius/log_metrics.tf
+++ modules/radius/log_metrics.tf
@@ -3,7 +3,7 @@ resource "aws_cloudwatch_log_metric_filter" "radius_request_filter" {
 
   name           = replace(each.value, ":", "")
   pattern        = format("\"%s\"", each.value)
-  log_group_name = aws_cloudwatch_log_group.server_log_group.name
+  log_group_name = aws_cloudwatch_log_group.server_performance_log_group.name
 
   metric_transformation {
     name          = replace(each.value, ":", "")
