34faa77447748510f144c873a400f209bd22e764
Emile <emile.swarts@digital.justice.gov.uk>
2022-02-15 11:18:50 +0000

    Revert explicit accept metric

--- modules/radius/log_metrics.tf
+++ modules/radius/log_metrics.tf
@@ -12,16 +12,3 @@ resource "aws_cloudwatch_log_metric_filter" "radius_request_filter" {
     default_value = "0"
   }
 }
-
-resource "aws_cloudwatch_log_metric_filter" "radius_request_filter" {
-  name           = "Sent Access-Accept"
-  pattern        = "Sent Access-Accept"
-  log_group_name = aws_cloudwatch_log_group.server_performance_log_group.name
-
-  metric_transformation {
-    name          = replace(each.value, ":", "")
-    namespace     = var.log_metrics_namespace
-    value         = "1"
-    default_value = "0"
-  }
-}
