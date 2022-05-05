7d17f206391b56aba42cc0ec9711d37ef39f3a63
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-05-05 15:25:46 +0100

    Add load_balancer_ip_address variable

--- main.tf
+++ main.tf
@@ -300,6 +300,7 @@ module "performance_testing" {
   prefix  = "${module.label.id}-perf"
   vpc_id  = module.radius_vpc.vpc_id
   subnets = module.radius_vpc.public_subnets
+  load_balancer_ip_address = module.radius.load_balancer.nac_eu_west_2a_ip_address
 
   providers = {
     aws = aws.env
