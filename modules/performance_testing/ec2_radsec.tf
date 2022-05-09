35f763902ac02643fc0711b18b33c0d90735ae81
Mitch Dawson <mitchell.dawson@justice.gov.uk>
2022-05-09 11:12:24 +0100

    :wrench: Add 10 EC2 Instances
    Commence testing with 10 EC2 instances.

--- modules/performance_testing/ec2_radsec.tf
+++ modules/performance_testing/ec2_radsec.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "performance_testing_instance_radsec" {
   ami           = "ami-07438ed9014cde68f"
   instance_type = "t4g.large"
-  count         = 0
+  count         = 10
 
   vpc_security_group_ids = [
     aws_security_group.performance_testing_instance.id
