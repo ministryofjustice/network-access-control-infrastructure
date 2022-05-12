c86bfa233abe60d501591bf73565905895b072dc
Emile <emile.swarts@digital.justice.gov.uk>
2022-05-12 18:44:14 +0100

    Create eventrule for unhealthy host count
    The default cloudwatch metric is unreliable

---
+++ modules/radius/unhealthy_host_count_event_rule.tf
@@ -0,0 +1,23 @@
+resource "aws_cloudwatch_event_rule" "unhealthy-host-count" {
+  name        = "${var.prefix}-unhealthy-host-count"
+  description = "Unhealthy host count metric"
+
+  event_pattern = <<EOF
+{
+    "source": [
+        "aws.ecs"
+    ],
+    "detail-type": [
+        "ECS Task State Change"
+    ],
+    "detail": {
+        "group": [
+            "service:${aws_ecs_service.service.id}"
+        ],
+        "stoppedReason": [
+            "Essential container in task exited"
+        ]
+    }
+}
+EOF
+}
