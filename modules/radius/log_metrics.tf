e2ee3553998798c91e6b350810e4e36e76eaac57
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-11-08 11:12:20 +0000

    :wrench: Update resource to account for complex queries
    This commit updates the aws_cloudwatch_log_metric_filter resource to account for the scenario where the log_filters list in main contains entries with queries. The first resource handles 'normal' elements of the list and the latter resource is setup to filter for entries containing '?' and create metrics based off of this.

--- modules/radius/log_metrics.tf
+++ modules/radius/log_metrics.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_metric_filter" "radius_request_filter" {
-  for_each = toset(var.log_filters)
+  for_each = toset([for i in var.log_filters : replace(i, ":", "") if !(length(regexall("\\?", i)) > 0) ])
 
   name           = replace(each.value, ":", "")
   pattern        = format("\"%s\"", each.value)
@@ -10,5 +10,21 @@ resource "aws_cloudwatch_log_metric_filter" "radius_request_filter" {
     namespace     = var.log_metrics_namespace
     value         = "1"
     default_value = "0"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "radius_request_filter_with_queries" {
+  for_each = toset([for i in var.log_filters : replace(i, ":", "") if length(regexall("\\?", i)) > 0 ])
+
+  name           = regex("[[:alpha:]]+", each.value)
+  pattern        = replace(each.value, "'", "\"")
+  log_group_name = aws_cloudwatch_log_group.server_log_group.name
+
+  metric_transformation {
+    name          = regex("[[:alpha:]]+", each.value)
+    namespace     = var.log_metrics_namespace
+    value         = "1"
+    default_value = "0"
+    unit          = "count"
   }
 }
