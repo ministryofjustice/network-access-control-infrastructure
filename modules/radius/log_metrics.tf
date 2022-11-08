a4a3842fcbf7d4276e3367c6f5fa6d38b65a27e4
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-11-08 16:43:11 +0000

    :pencil2: Update phrase

--- modules/radius/log_metrics.tf
+++ modules/radius/log_metrics.tf
@@ -16,12 +16,12 @@ resource "aws_cloudwatch_log_metric_filter" "radius_request_filter" {
 resource "aws_cloudwatch_log_metric_filter" "radius_request_filter_with_queries" {
   for_each = toset([for i in var.log_filters : replace(i, ":", "") if length(regexall("\\?", i)) > 0])
 
-  name           = length(regexall("error", each.value)) > 0 ? "All other errors" : regex("[[:alpha:]]+", each.value)
+  name           = length(regexall("error", each.value)) > 0 ? "All errors" : regex("[[:alpha:]]+", each.value)
   pattern        = replace(each.value, "'", "\"")
   log_group_name = aws_cloudwatch_log_group.server_log_group.name
 
   metric_transformation {
-    name          = length(regexall("error", each.value)) > 0 ? "All other errors" : regex("[[:alpha:]]+", each.value)
+    name          = length(regexall("error", each.value)) > 0 ? "All errors" : regex("[[:alpha:]]+", each.value)
     namespace     = var.log_metrics_namespace
     value         = "1"
     default_value = "0"
