20762f940c360c6ee2d1ac36f677da635885e80d
Gary <26419401+Gary-H9@users.noreply.github.com>
2022-12-13 15:31:34 +0000

    :pencil2: Fix typo (#215)
    * :pencil2: Fix typo
    
    * :pencil2: Indentation

--- modules/radius/ecr_nginx.tf
+++ modules/radius/ecr_nginx.tf
@@ -8,14 +8,14 @@ resource "aws_ecr_repository_policy" "nginx" {
         {
             "Sid": "1",
             "Effect": "Allow",
-            "Principal":
+            "Principal":{
                 "AWS": [
-                        "683290208331",
-                        "068084030754",
-                        "473630360727",
-                        "037161842252"
-                        ]
-                    },
+                    "683290208331",
+                    "068084030754",
+                    "473630360727",
+                    "037161842252"
+                    ]
+                },
             "Action": [
                 "ecr:GetDownloadUrlForLayer",
                 "ecr:BatchGetImage",
