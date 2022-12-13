89dcfb5b279a31691f3d831e2338c56747cf0c36
Gary <26419401+Gary-H9@users.noreply.github.com>
2022-12-13 15:21:33 +0000

    :lock: Update ECR IAM Policy (#214)

--- modules/radius/ecr.tf
+++ modules/radius/ecr.tf
@@ -19,7 +19,14 @@ resource "aws_ecr_repository_policy" "radius" {
         {
             "Sid": "1",
             "Effect": "Allow",
-            "Principal": "*",
+            "Principal":
+                "AWS": [
+                    "683290208331",
+                    "068084030754",
+                    "473630360727",
+                    "037161842252"
+                    ]
+                },
             "Action": [
                 "ecr:GetDownloadUrlForLayer",
                 "ecr:BatchGetImage",
