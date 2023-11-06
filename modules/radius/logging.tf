1956c5bdbd346d4df3097c1999219fe4753677f3
satishgummadellimoj <134059962+satishgummadellimoj@users.noreply.github.com>
2023-11-06 14:11:14 +0000

    updating log retention to 90 days (#230)

--- modules/radius/logging.tf
+++ modules/radius/logging.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_log_group" "server_log_group" {
   name = "${var.prefix}-server-log-group"
 
-  retention_in_days = 7
+  retention_in_days = 90
 
   tags = var.tags
 }
@@ -9,7 +9,7 @@ resource "aws_cloudwatch_log_group" "server_log_group" {
 resource "aws_cloudwatch_log_group" "server_nginx_log_group" {
   name = "${var.prefix}-server-nginx-log-group"
 
-  retention_in_days = 7
+  retention_in_days = 90
 
   tags = var.tags
 }
