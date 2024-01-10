2c186b76ec2ec284f21d058e52e27d20ba9db9fe
Jahir <124062934+juddin927@users.noreply.github.com>
2024-01-10 11:19:45 +0000

    added a script to add cloudwatch log retention to 90 days for the RDS log group,as the default is set by the AWS and no configuration option specified elsewhere (#252)
    Co-authored-by: Jahir <jahir.uddin@digital.justice.gov.uk>

--- scripts/terraform_plan_or_apply.sh
+++ scripts/terraform_plan_or_apply.sh
@@ -7,7 +7,3 @@ if [[ "${PLAN}" == "true" ]]; then
 else
   terraform apply --auto-approve -no-color
 fi
-# Run the cloud watch log retention script if in production and apply was successful
-if [[ $ENV == "production" ]] && [[ $? -eq 0 ]]; then
-  bash ./scripts/cloudwatch_log_retention_policies.sh
-fi
