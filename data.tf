c2ce7fa126012ed02c03ecf5f76a1ebd38ecd904
jamesgreen-moj <144033531+jamesgreen-moj@users.noreply.github.com>
2024-02-02 16:00:10 +0000

    Xsiam firehose (#259)
    * firehose module for xsiam logs ingestion
    
    * Added terraform force unlock
    
    * adjusted iam polcies, and module to main.tf
    
    * Added Cloudwatch error logging
    
    Added config so we can view Cloudwatch logging errors via cloudwatch
    
    * updated module to use secrets manager via data
    
    AWS secrets mananger now store the http endpoint and access_key for the
    firehose http endpoint.
    
    * Added server_side_enrytion to firehose
    
    * added logging group resource and permissions for firehose to log to it
    
    * added cloudwatch subscription
    
    * Commit changes made by code formatters
    
    * renamed the logs subscription name to reflect nacs to xsiam
    
    * updated secret version for xsiam endpoint in production and pre-production
    
    * updated secret version for xsiam endpoint in development,production and pre-production
    
    * locked down permissive permissions
    
    ---------
    
    Co-authored-by: Jahir <jahir.uddin@digital.justice.gov.uk>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- data.tf
+++ data.tf
@@ -1,3 +1,9 @@
+locals {
+  xaiam_secrets_version_development    = "2e73a1de-af34-4c1d-a8ce-759df5b7bf75"
+  xaiam_secrets_version_pre_production = "9a071db2-4ed2-4c3f-9568-5ef2d5299dc4"
+  xaiam_secrets_version_production     = "a275ae6e-fc4c-4341-bb63-064f4e2fe209"
+}
+
 #-----------------------------------------------------------------
 ### Getting the staff-device-shared-services-infrastructure state
 #-----------------------------------------------------------------
@@ -9,4 +15,13 @@ data "terraform_remote_state" "staff-device-shared-services-infrastructure" {
     key    = "env:/ci/terraform/v1/state"
     region = "eu-west-2"
   }
+}
+
+data "aws_secretsmanager_secret" "xsiam_endpoint_secrets" {
+  name = "/nac-server/${terraform.workspace}/xsiam_endpoint_secrets"
+}
+
+data "aws_secretsmanager_secret_version" "xaiam_secrets_version" {
+  secret_id  = data.aws_secretsmanager_secret.xsiam_endpoint_secrets.id
+  version_id = terraform.workspace == "pre-production" ? local.xaiam_secrets_version_pre_production : terraform.workspace == "production" ? local.xaiam_secrets_version_production : local.xaiam_secrets_version_development
 }
