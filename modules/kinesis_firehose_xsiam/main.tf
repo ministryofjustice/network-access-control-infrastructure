2db90fd5a6d4f9bfb2850abad3b28eebed738521
Jahir <jahir.uddin@digital.justice.gov.uk>
2024-02-05 11:27:34 +0000

    added tagging for firehose cloudwatch(error) log groups

--- modules/kinesis_firehose_xsiam/main.tf
+++ modules/kinesis_firehose_xsiam/main.tf
@@ -32,8 +32,8 @@ resource "aws_kinesis_firehose_delivery_stream" "xsiam_delivery_stream" {
 }
 
 resource "aws_cloudwatch_log_group" "xsiam_delivery_group" {
-  name = "xsiam-delivery-stream-${var.prefix}"
-
+  name              = "xsiam-delivery-stream-${var.prefix}"
+  tags              = var.tags
   retention_in_days = 90
 }
 
