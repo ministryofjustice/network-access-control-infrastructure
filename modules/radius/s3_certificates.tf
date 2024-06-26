0eb43a9038ed9699913ffb2ff9d40c6bb29271ba
lauren_tb <lauren.taylor-brown@justice.gov.uk>
2024-06-26 16:06:35 +0100

    removed un-needed resource

--- modules/radius/s3_certificates.tf
+++ modules/radius/s3_certificates.tf
@@ -1,15 +1,6 @@
 resource "aws_s3_bucket" "certificate_bucket" {
   bucket = "${var.prefix}-certificate-bucket"
 
-  # server_side_encryption_configuration {
-  #   rule {
-  #     apply_server_side_encryption_by_default {
-  #       kms_master_key_id = aws_kms_key.certificate_bucket_key.arn
-  #       sse_algorithm     = "aws:kms"
-  #     }
-  #   }
-  # }
-
   tags = var.tags
 }
 
