b68a21230dda9cf2f69c007a5abbc8287ddfde05
Stephen.James <stephen.james4@digital.justice.gov.uk>
2024-07-01 11:17:57 +0100

    Removed ami parameter The default value is useful for all bastion version now.
    ND-403

--- bastion-rds-servers.tf
+++ bastion-rds-servers.tf
@@ -11,7 +11,6 @@ module "rds_servers_bastion" {
   vpc_cidr_block              = module.radius_vpc.vpc.vpc_cidr_block
   private_subnets             = module.radius_vpc.private_subnets
   security_group_ids          = [module.radius.security_group_ids.radius_server]
-  ami_name                    = "diso-devops/bastion/rds-admin/ubuntu-jammy-22.04-amd64-server-1.0.1"
   number_of_bastions          = 1
   assume_role                 = local.s3-mojo_file_transfer_assume_role_arn
   associate_public_ip_address = false
