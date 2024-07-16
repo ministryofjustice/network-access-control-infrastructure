793c757e9ae5d7c16359d918d22cef7dcfc85872
Stephen.James <stephen.james4@digital.justice.gov.uk>
2024-07-16 14:44:16 +0100

    Moved the common changes to the default resources array
    Added the items that are different per env to their own array which will be merged.
    Place holders for the pre-production and production arrays.

--- scripts/tf_mv/ND-134-vpc-module.sh
+++ scripts/tf_mv/ND-134-vpc-module.sh
@@ -3,9 +3,7 @@
 echo "Script running is $(basename "$0")"
 echo "Env is ${ENV}"
 
-declare -A RESOURCES=()
-
-declare -A RESOURCES_DEVELOPMENT=(
+declare -A RESOURCES=(
     ["module.radius_vpc.module.vpc.aws_vpc_endpoint.rds[0]"]='module.radius_vpc.aws_vpc_endpoint.rds'
     ["module.radius_vpc.module.vpc.aws_vpc_endpoint.s3[0]"]='module.radius_vpc.aws_vpc_endpoint.s3'
     ["module.radius_vpc.module.vpc.aws_vpc_endpoint.logs[0]"]='module.radius_vpc.aws_vpc_endpoint.logs'
@@ -13,15 +11,24 @@ declare -A RESOURCES_DEVELOPMENT=(
     ["module.radius_vpc.module.vpc.aws_vpc_endpoint.ecr_dkr[0]"]='module.radius_vpc.aws_vpc_endpoint.ecr_dkr'
     ["module.radius_vpc.module.vpc.aws_vpc_endpoint.ecr_api[0]"]='module.radius_vpc.aws_vpc_endpoint.ecr_api'
     ["module.radius_vpc.module.vpc.aws_vpc_endpoint_route_table_association.public_s3[0]"]='module.radius_vpc.aws_vpc_endpoint_route_table_association.public_s3'
+)
+
+declare -A RESOURCES_DEVELOPMENT=(
     ["module.radius_vpc.module.vpc.aws_vpc_endpoint_route_table_association.private_s3[0]"]='module.radius_vpc.aws_vpc_endpoint_route_table_association.private_s3["rtb-0f4597542e6aa0556"]'
     ["module.radius_vpc.module.vpc.aws_vpc_endpoint_route_table_association.private_s3[1]"]='module.radius_vpc.aws_vpc_endpoint_route_table_association.private_s3["rtb-031a61b9efe154408"]'
     ["module.radius_vpc.module.vpc.aws_vpc_endpoint_route_table_association.private_s3[2]"]='module.radius_vpc.aws_vpc_endpoint_route_table_association.private_s3["rtb-0b23013f2990bd5f5"]'
 )
 
 declare -A RESOURCES_PRE_PRODUCTION=(
+    ["module.radius_vpc.module.vpc.aws_vpc_endpoint_route_table_association.private_s3[0]"]='module.radius_vpc.aws_vpc_endpoint_route_table_association.private_s3["rtb-XXX"]'
+    ["module.radius_vpc.module.vpc.aws_vpc_endpoint_route_table_association.private_s3[1]"]='module.radius_vpc.aws_vpc_endpoint_route_table_association.private_s3["rtb-YYY"]'
+    ["module.radius_vpc.module.vpc.aws_vpc_endpoint_route_table_association.private_s3[2]"]='module.radius_vpc.aws_vpc_endpoint_route_table_association.private_s3["rtb-ZZZ"]'
 )
 
 declare -A RESOURCES_PRODUCTION=(
+    ["module.radius_vpc.module.vpc.aws_vpc_endpoint_route_table_association.private_s3[0]"]='module.radius_vpc.aws_vpc_endpoint_route_table_association.private_s3["rtb-AAA"]'
+    ["module.radius_vpc.module.vpc.aws_vpc_endpoint_route_table_association.private_s3[1]"]='module.radius_vpc.aws_vpc_endpoint_route_table_association.private_s3["rtb-BBB"]'
+    ["module.radius_vpc.module.vpc.aws_vpc_endpoint_route_table_association.private_s3[2]"]='module.radius_vpc.aws_vpc_endpoint_route_table_association.private_s3["rtb-CCC"]'
 )
 
 printf "\n\nEnvironment is %s\n\n" "${ENV}"
