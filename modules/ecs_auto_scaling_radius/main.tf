846ebf4f02b916124162d837b2887981dde08a2f
lauren_tb <lauren.taylor-brown@justice.gov.uk>
2024-08-08 14:55:31 +0100

    removed duplicated scale policy

--- modules/ecs_auto_scaling_radius/main.tf
+++ modules/ecs_auto_scaling_radius/main.tf
@@ -244,11 +244,11 @@ resource "aws_cloudwatch_metric_alarm" "ecs_memory_maximum_alarm_high" {
 }
 
 ###########################################################################
-#                 CPU UTILIZATION ALARM AND SCALE POLICY
+#                        CPU UTILIZATION ALARM
 ###########################################################################
 
 resource "aws_cloudwatch_metric_alarm" "cpu_utilization_alarm" {
-  alarm_name          = "${var.prefix}-cpu-utilization-alarm"
+  alarm_name          = "${var.prefix}-ecs-cpu-utilization-alarm"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "1"
   metric_name         = "CPUUtilization"
@@ -265,29 +265,9 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization_alarm" {
   alarm_description = "This alarm tells ECS to scale up based on CPU utilisation with Average statistics"
 
   alarm_actions = [
-    aws_appautoscaling_policy.ecs_policy_up_cpu_util.arn
+    aws_appautoscaling_policy.ecs_policy_up.arn
   ]
 
   treat_missing_data = "breaching"
   tags               = var.tags
-}
-
-resource "aws_appautoscaling_policy" "ecs_policy_up_cpu_util" {
-  name               = "${var.prefix} ECS Scale Up CPU Utilization"
-  service_namespace  = "ecs"
-  policy_type        = "StepScaling"
-  resource_id        = "service/${var.cluster_name}/${var.service_name}"
-  scalable_dimension = "ecs:service:DesiredCount"
-
-  step_scaling_policy_configuration {
-    adjustment_type         = "ChangeInCapacity"
-    metric_aggregation_type = "Average"
-
-    step_adjustment {
-      metric_interval_lower_bound = 0
-      scaling_adjustment          = 1
-    }
-  }
-
-  depends_on = [aws_appautoscaling_target.radius]
 }
