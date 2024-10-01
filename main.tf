c712ad1070d1e3d178f3e0d43dcde6513829ec23
Stephen.James <stephen.james4@digital.justice.gov.uk>
2024-10-01 11:30:10 +0100

    Refactored locals into their own file
    ND-533

--- main.tf
+++ main.tf
@@ -34,29 +34,5 @@ module "label" {
 }
 
 
-locals {
-  private_ip_eu_west_2a   = "10.180.108.10"
-  private_ip_eu_west_2b   = "10.180.109.10"
-  private_ip_eu_west_2c   = "10.180.110.10"
-  vpc_cidr                = "10.180.108.0/22"
-  is_production           = terraform.workspace == "production" ? true : false
-  is_pre_production       = terraform.workspace == "pre-production" ? true : false
-  is_development          = terraform.workspace == "development" ? true : false
-  is_local_development    = !local.is_development && !local.is_pre_production && !local.is_production
-  run_restore_from_backup = false
-
-  s3-mojo_file_transfer_assume_role_arn = data.terraform_remote_state.staff-device-shared-services-infrastructure.outputs.s3-mojo_file_transfer_assume_role_arn
-}
-
-
-
-
-
-
-
-
-
-
-
 data "aws_region" "current_region" {}
 data "aws_caller_identity" "shared_services_account" {}
