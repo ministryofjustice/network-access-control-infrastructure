dbe215973361da0b4c871de8b3d1442320634e1a
Stephen.James <stephen.james4@digital.justice.gov.uk>
2024-10-01 11:04:54 +0100

    Refactor performance_testing into onw file
    ND-533

--- main.tf
+++ main.tf
@@ -302,16 +302,3 @@ module "admin_vpc_flow_logs" {
     aws = aws.env
   }
 }
-
-module "performance_testing" {
-  source                   = "./modules/performance_testing"
-  count                    = local.is_development ? 1 : 0
-  prefix                   = "${module.label.id}-perf"
-  vpc_id                   = module.radius_vpc.vpc_id
-  subnets                  = module.radius_vpc.public_subnets
-  load_balancer_ip_address = module.radius.load_balancer.nac_eu_west_2a_ip_address
-
-  providers = {
-    aws = aws.env
-  }
-}
