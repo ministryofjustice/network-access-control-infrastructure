c7c29924cae043f3de2f812b72d3b1adb3625a5c
Stephen.James <stephen.james4@digital.justice.gov.uk>
2024-10-01 11:07:48 +0100

    Refactor admin_vpc into own file
    ND-533

--- main.tf
+++ main.tf
@@ -277,28 +277,3 @@ module "authentication" {
 
 data "aws_region" "current_region" {}
 data "aws_caller_identity" "shared_services_account" {}
-
-module "admin_vpc" {
-  source                        = "./modules/admin_vpc"
-  prefix                        = "${module.label.id}-admin"
-  region                        = data.aws_region.current_region.id
-  cidr_block                    = "10.0.0.0/16"
-  tags                          = module.label.tags
-  ssm_session_manager_endpoints = var.enable_rds_admin_bastion
-
-  providers = {
-    aws = aws.env
-  }
-}
-
-module "admin_vpc_flow_logs" {
-  source = "./modules/vpc_flow_logs"
-  prefix = "${module.label.id}-admin-vpc-flow-logs"
-  region = data.aws_region.current_region.id
-  tags   = module.label.tags
-  vpc_id = module.admin_vpc.vpc_id
-
-  providers = {
-    aws = aws.env
-  }
-}
