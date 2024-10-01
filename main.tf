a33549937f62291b9c186f59adabc75bb602db6a
Stephen.James <stephen.james4@digital.justice.gov.uk>
2024-10-01 11:12:47 +0100

    Refactored authentication into it's own file
    ND-533

--- main.tf
+++ main.tf
@@ -227,19 +227,7 @@ module "admin" {
   }
 }
 
-module "authentication" {
-  source                        = "./modules/cognito"
-  azure_federation_metadata_url = var.azure_federation_metadata_url
-  prefix                        = module.label.id
-  enable_authentication         = var.enable_authentication
-  admin_url                     = module.admin.admin_url
-  region                        = data.aws_region.current_region.id
-  hosted_zone_domain            = var.hosted_zone_domain
 
-  providers = {
-    aws = aws.env
-  }
-}
 
 data "aws_region" "current_region" {}
 data "aws_caller_identity" "shared_services_account" {}
