e13c893e1cd8f2a33a9d981f51845c6633ce6d84
Asif Amirat <asif.amirat@justice.gov.uk>
2024-10-01 14:52:31 +0100

    updating - service_radius_admin_read_replica.tf - using data lookup for admin read replica db password to retrieve values directly from secrets manager

--- service_radius_admin_read_replica.tf
+++ service_radius_admin_read_replica.tf
@@ -5,7 +5,7 @@ module "admin_read_replica" {
   subnet_ids                      = module.radius_vpc.private_subnets
   rds_monitoring_role             = module.admin.rds.rds_monitoring_role
   vpc_id                          = module.radius_vpc.vpc_id
-  db_password                     = var.admin_db_password
+  db_password                     = jsondecode(data.aws_secretsmanager_secret_version.moj_network_access_control_env_admin_db.secret_string)["password"]
   db_size                         = "db.t3.large"
   radius_server_security_group_id = module.radius.ec2.radius_server_security_group_id
   prefix                          = "${module.label.id}-admin-read-replica"
