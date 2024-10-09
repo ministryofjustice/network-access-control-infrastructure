69ad49a21f5a825e3b582509290a611df29946cf
Stephen.James <stephen.james4@digital.justice.gov.uk>
2024-10-09 12:32:48 +0100

    Added vpc enspoints for secrets
    Also set count to 1 permanently for endpoints that now are required to persist.
    Using a hard coded count to prevent destruction and recreation of exisitng endpoints created by the
    "var.ssm_session_manager_endpoints" boolean when set to true.
    
    ND-510

--- modules/admin_vpc/endpoints.tf
+++ modules/admin_vpc/endpoints.tf
@@ -11,8 +11,21 @@ resource "aws_vpc_endpoint" "s3" {
 
 // endpoints required for session manager
 
+// endpoint required for bastions and ecs task get ssm parameters & secrets manager secrets
+
+resource "aws_vpc_endpoint" "secrets" {
+  vpc_id              = module.vpc.vpc_id
+  subnet_ids          = module.vpc.private_subnets
+  service_name        = "com.amazonaws.${var.region}.secretsmanager"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [aws_security_group.endpoints.id]
+  private_dns_enabled = true
+  tags                = var.tags
+  depends_on          = [aws_security_group.endpoints]
+}
+
 resource "aws_vpc_endpoint" "ssm" {
-  count               = var.ssm_session_manager_endpoints ? 1 : 0
+  count               = 1 #var.ssm_session_manager_endpoints ? 1 : 0
   vpc_id              = module.vpc.vpc_id
   subnet_ids          = module.vpc.private_subnets
   service_name        = "com.amazonaws.${var.region}.ssm"
@@ -24,7 +37,7 @@ resource "aws_vpc_endpoint" "ssm" {
 }
 
 resource "aws_vpc_endpoint" "ssmmessages" {
-  count               = var.ssm_session_manager_endpoints ? 1 : 0
+  count               = 1 #var.ssm_session_manager_endpoints ? 1 : 0
   vpc_id              = module.vpc.vpc_id
   subnet_ids          = module.vpc.private_subnets
   service_name        = "com.amazonaws.${var.region}.ssmmessages"
